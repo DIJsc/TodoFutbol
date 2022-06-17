249136632b780ce969046f753f7305e76f06a9d8
Joako3555 <steinbachjoakimiep@gmial.com>
2022-06-17 10:09:48 -0300

    js v2

--- js/contador.js
+++ js/contador.js
@@ -1,19 +1,19 @@
 var contador = new Date("Nov 21, 2022 07:00:00").getTime();
 var funcion = setInterval(function() {
-    var now = new Date().getTime();
-    var timeleft = countDownDate - now;
+    var hoy = new Date().getTime();
+    var tiemporestante = contador - hoy;
         
-    // Calculating the days, hours, minutes and seconds left
-    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+   
+    var days = Math.floor(tiemporestante / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((tiemporestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((tiemporestante % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((tiemporestante % (1000 * 60)) / 1000);
     if (timeleft > 0) {
-    // Result is output to the specific element
-    document.getElementById("days").innerHTML = days 
-    document.getElementById("hours").innerHTML = hours 
-    document.getElementById("mins").innerHTML = minutes
-    document.getElementById("seconds").innerHTML = seconds
+  
+    document.getElementById("dias").innerHTML = days 
+    document.getElementById("horas").innerHTML = hours 
+    document.getElementById("min").innerHTML = minutes
+    document.getElementById("segundos").innerHTML = seconds
    } 
     }, 1000);
 
