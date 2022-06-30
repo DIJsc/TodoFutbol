04d70d0a75dd658e63a4af9346d84ef2ee929deb
Joako3555 <steinbachjoakimiep@gmial.com>
2022-06-30 11:22:48 -0300

    a

---
+++ js/verificacionpassw.js
@@ -0,0 +1,26 @@
+function verificarPasswords() {
+ 
+    pass1 = document.getElementById('passw1');
+    pass2 = document.getElementById('passw2');
+ 
+    if (pass1.value != pass2.value) {
+ 
+        document.getElementById("error").classList.add("mostrar");
+ 
+        return false;
+    } else {
+ 
+        document.getElementById("error").classList.remove("mostrar");
+ 
+        document.getElementById("ok").classList.remove("ocultar");
+ 
+        document.getElementById("login").disabled = true;
+ 
+        setTimeout(function() {
+            location.reload();
+        }, 3000);
+ 
+        return true;
+    }
+ 
+}
