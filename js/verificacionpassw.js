f883eac13e4e6d2685d66572de172ff6c404c6bd
Joako3555 <steinbachjoakimiep@gmial.com>
2022-07-01 08:58:50 -0300

    k

--- js/verificacionpassw.js
+++ js/verificacionpassw.js
@@ -1,20 +1,18 @@
 function verificarPasswords() {
  
-    pass1 = document.getElementById('passw1');
-    pass2 = document.getElementById('passw2');
+    pass1 = document.getElementById('pass1');
+    pass2 = document.getElementById('pass2');
  
-    if (pass1.value != pass2.value) {
- 
-        document.getElementById("error").classList.add("mostrar");
- 
+    if (formulario.pass1.value != formulario.pass2.value ) {
+        
+        document.getElementById("alerta").innerHTML = '<div class="alert alert-danger"><a href="" class="close" data-dismiss="alert">&times;</a> contraseñas ingresadas no coinciden. </div>';
+        formulario.pass1.value = "";
+        formulario.pass2.value = "";
+        formulario.pass1.focus();
         return false;
     } else {
  
-        document.getElementById("error").classList.remove("mostrar");
- 
-        document.getElementById("ok").classList.remove("ocultar");
- 
-        document.getElementById("login").disabled = true;
+        document.getElementById("alerta").innerHTML = "";
  
         setTimeout(function() {
             location.reload();
